18a877a3695c60c880494a27ecd049440a7ba8f0
Ebanx3 <esteban.dos.santos@gmail.com>
2024-10-25 21:19:06 -0300

    Language system implemented

---
+++ Assets/Scripts/Language.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+public class Language : MonoBehaviour
+{
+    public GameObject languageDropdown;
+    private TMP_Dropdown dropdown;
+
+    void Awake(){
+        dropdown = languageDropdown.GetComponent<TMP_Dropdown>();
+        dropdown.value = LocalizationSettings.SelectedLocale.SortOrder;
+    }
+
+    public void ChangeLanguage(int index)
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
+}
--- Assets/Scripts/UI/TogglePanel.cs
+++ Assets/Scripts/UI/TogglePanel.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
+
 public class TogglePanel : MonoBehaviour
 {
-    public GameObject panel; // Asigna aquí el Panel desde el editor
+    public GameObject panel; // Asigna aquï¿½ el Panel desde el editor
 
-    // Método para alternar la visibilidad del panel
+    // Mï¿½todo para alternar la visibilidad del panel
     public void TogglePanelVisibility()
     {
         panel.SetActive(!panel.activeSelf); // Cambia el estado activo del panel (mostrar/ocultar)
     }
+
+    
 }
